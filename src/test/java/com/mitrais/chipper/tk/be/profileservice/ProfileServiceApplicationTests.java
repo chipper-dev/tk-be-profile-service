9aa9a013c62a5f8aa12a48914291193a4da60a11
akbarb24 <akbar.binaji@gmail.com>
2020-07-07 12:11:02 +0800

    init

---
+++ src/test/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitrais.chipper.tk.be.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
