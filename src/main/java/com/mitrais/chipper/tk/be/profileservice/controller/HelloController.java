3fbf40a56a0701deac459df93c364dac43a72847
akbarb24 <akbar.binaji@gmail.com>
2020-07-13 09:57:06 +0800

    add hellocontroller

---
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.mitrais.chipper.tk.be.profileservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "hello";
+    }
+}
