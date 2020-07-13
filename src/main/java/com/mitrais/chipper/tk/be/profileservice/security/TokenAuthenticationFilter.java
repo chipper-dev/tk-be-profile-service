ed1be04365fcb0aa63263468470a35c7aa93fb98
akbarb24 <akbar.binaji@gmail.com>
2020-07-13 16:59:49 +0800

    refactor TokenAuthenticationFilter

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/security/TokenAuthenticationFilter.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/security/TokenAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.mitrais.chipper.tk.be.profileservice.security;
 
 import com.mitrais.chipper.tk.be.profileservice.config.JwtConfig;
-import io.jsonwebtoken.*;
+import com.mitrais.chipper.tk.be.profileservice.interfaces.UserInterface;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,58 +16,48 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final JwtConfig jwtConfig;
+    private final UserInterface userInterface;
+    private final TokenProvider tokenProvider;
 
-    public TokenAuthenticationFilter(JwtConfig jwtConfig) {
+    public TokenAuthenticationFilter(JwtConfig jwtConfig, UserInterface userInterface, TokenProvider tokenProvider) {
         this.jwtConfig = jwtConfig;
+        this.userInterface = userInterface;
+        this.tokenProvider = tokenProvider;
     }
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        // 1. get the authentication header. Tokens are supposed to be passed in the authentication header
         String header = request.getHeader(jwtConfig.getHeader());
 
         if(header == null || !header.startsWith(jwtConfig.getPrefix())) {
-            chain.doFilter(request, response);  		// If not valid, go to the next filter.
+            chain.doFilter(request, response);
             return;
         }
 
-
         String token = header.replace(jwtConfig.getPrefix(), "");
 
-        try {	// exceptions might be thrown in creating the claims if for example the token is expired
-
-            // 4. Validate the token
-            Claims claims = Jwts.parser()
-                    .setSigningKey(jwtConfig.getSecret())
-                    .parseClaimsJws(token)
-                    .getBody();
-            String userId = claims.getSubject();
-
+        try {
+            Long userId = tokenProvider.getUserIdFromToken(token);
             if(userId != null) {
-                List<GrantedAuthority> authorities = Collections.
-                        singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                Date logoutTime = userInterface.fetchUserbyId(userId).orElseThrow(() -> new NoSuchElementException("User not found")).getLogout();
+                if (tokenProvider.validateToken(token, logoutTime)) {
 
-                // 5. Create auth object
-                // UsernamePasswordAuthenticationToken: A built-in object, used by spring to represent the current authenticated / being authenticated user.
-                // It needs a list of authorities, which has type of GrantedAuthority interface, where SimpleGrantedAuthority is an implementation of that interface
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                        userId, null, authorities);
-
-                // 6. Authenticate the user
-                // Now, user is authenticated
-                SecurityContextHolder.getContext().setAuthentication(auth);
+                    List<GrantedAuthority> authorities = Collections.
+                            singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                            userId, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
             }
 
         } catch (Exception e) {
-            // In case of failure. Make sure it's clear; so guarantee user won't be authenticated
             SecurityContextHolder.clearContext();
         }
 
-        // go to the next filter in the filter chain
         chain.doFilter(request, response);
     }
 }
