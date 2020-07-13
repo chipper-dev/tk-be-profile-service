d031d045576ad5142ece23ebb13d28eb2bdee6b0
akbarb24 <akbar.binaji@gmail.com>
2020-07-13 16:59:25 +0800

    enable feign client

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
