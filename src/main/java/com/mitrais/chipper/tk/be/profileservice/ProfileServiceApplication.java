e194ca6689471c9b2c98caffb761e674d938c977
Doni K Rizky <donikrizky@gmail.com>
2020-08-12 13:11:44 +0700

    change pom

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
@@ -3,13 +3,7 @@ package com.mitrais.chipper.tk.be.profileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableFeignClients
 @EnableEurekaClient
