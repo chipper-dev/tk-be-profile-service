2ce144d50883130529c859c3eedbb3fcc99eb9da
Doni K Rizky <donikrizky@gmail.com>
2020-08-12 12:25:59 +0700

    add annotation to main class

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/ProfileServiceApplication.java
@@ -4,19 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableFeignClients
+//@EnableFeignClients
 @EnableEurekaClient
+@EnableDiscoveryClient
 //@EnableCircuitBreaker
 //@EnableHystrixDashboard
 //@EnableHystrix
 @SpringBootApplication
-//@EnableAutoConfiguration
+@EnableAutoConfiguration
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ProfileServiceApplication {
 
