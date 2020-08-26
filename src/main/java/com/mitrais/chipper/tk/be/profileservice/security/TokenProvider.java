2a05de339ec91e5c77603e90bd1e8b7612c8a39b
Doni K Rizky <donikrizky@gmail.com>
2020-08-26 10:46:07 +0700

    erase autowired for jwtconfig

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/security/TokenProvider.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/security/TokenProvider.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.mitrais.chipper.tk.be.profileservice.config.JwtConfig;
@@ -18,36 +17,32 @@ import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class TokenProvider {
-    private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+	private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
 
-    @Autowired
-    private JwtConfig jwtConfig;
+	private JwtConfig jwtConfig;
 
-    public Long getUserIdFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtConfig.getSecret())
-                .parseClaimsJws(token)
-                .getBody();
-        return Long.parseLong(claims.getSubject());
-    }
+	public Long getUserIdFromToken(String token) {
+		Claims claims = Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
+	}
 
-    public boolean validateToken(String authToken, Date logoutTime) {
-        try {
-            Claims claims = Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(authToken).getBody();
-            if(logoutTime == null || claims.getIssuedAt().after(logoutTime)){
-                return true;
-            }
-        } catch (SignatureException ex) {
-            logger.error("Invalid JWT signature");
-        } catch (MalformedJwtException ex) {
-            logger.error("Invalid JWT token");
-        } catch (ExpiredJwtException ex) {
-            logger.error("Expired JWT token");
-        } catch (UnsupportedJwtException ex) {
-            logger.error("Unsupported JWT token");
-        } catch (IllegalArgumentException ex) {
-            logger.error("JWT claims string is empty.");
-        }
-        return false;
-    }
+	public boolean validateToken(String authToken, Date logoutTime) {
+		try {
+			Claims claims = Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(authToken).getBody();
+			if (logoutTime == null || claims.getIssuedAt().after(logoutTime)) {
+				return true;
+			}
+		} catch (SignatureException ex) {
+			logger.error("Invalid JWT signature");
+		} catch (MalformedJwtException ex) {
+			logger.error("Invalid JWT token");
+		} catch (ExpiredJwtException ex) {
+			logger.error("Expired JWT token");
+		} catch (UnsupportedJwtException ex) {
+			logger.error("Unsupported JWT token");
+		} catch (IllegalArgumentException ex) {
+			logger.error("JWT claims string is empty.");
+		}
+		return false;
+	}
 }
