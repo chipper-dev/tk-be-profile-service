61c04419826b74658fdda699ee70948a62e929a4
Doni K Rizky <donikrizky@gmail.com>
2020-08-26 12:10:15 +0700

    migrate controller

--- src/main/java/com/mitrais/chipper/tk/be/profileservice/controller/MigrateController.java
+++ src/main/java/com/mitrais/chipper/tk/be/profileservice/controller/MigrateController.java
@@ -4,15 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mitrais.chipper.tk.be.profileservice.common.CommonResource;
+import com.mitrais.chipper.tk.be.profileservice.common.ResponseBody;
+import com.mitrais.chipper.tk.be.profileservice.security.TokenProvider;
+import com.mitrais.chipper.tk.be.profileservice.service.MigrateService;
 
 @RestController
 @Validated
 @RequestMapping("/migrate")
-public class MigrateController {
+public class MigrateController extends CommonResource {
+
+	@Autowired
+	TokenProvider tokenProvider;
+
+	@Autowired
+	MigrateService migrateService;
 
 	@Autowired
 	private Environment env;
@@ -22,4 +37,14 @@ public class MigrateController {
 		return "Hello from Profile Service running at port: " + env.getProperty("local.server.port");
 	}
 	
+	@PostMapping("/fetchAndSaveDataFromLegacy")
+	public ResponseEntity<ResponseBody> fetchDBFromLegacy(HttpServletRequest request) {
+		LOGGER.info("Fetch and save all data from legacy profile to microservice profile");
+		String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+		Long userId = tokenProvider.getUserIdFromToken(getToken(header));
+
+		migrateService.fetchAndSaveFromLegacy(header, userId);
+		return ResponseEntity.ok(getResponseBody(HttpStatus.OK.value(), "You have fetch and save all data from legacy profile",
+				request.getRequestURI()));
+	}
 }
